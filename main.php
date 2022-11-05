ae115d787734899a3e0a00de67eefed1949af3d9
Marvish Subhas Chandra <mschandr@uci.edu>
2022-11-05 08:38:01 -0700

    Add files via upload

---
+++ main.php
@@ -0,0 +1,31 @@
+// Marvish Chandra
+
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Top Rome Destinations</h1>
+
+<?php
+$firstDestination = "Roman Colosseum";
+echo $firstDestination;
+$secondDestination = "Roman Forum";
+echo $secondDestination;
+$thirdDestination = "Piazza Navona";
+echo $thirdDestination;
+$fourthDestination = "Trevi Fountain"
+echo $fourthDestination;
+$fifthDestination = "St. Peter's Square";
+echo $fifthDestination;
+$sixthDestination = "St. Peter's Basilica";
+echo $sixthDestination;
+$seventhDestination = "Sistine Chapel";
+echo $seventhDestination;
+$eighthDestination = "Piazza di Spagna & the Spanish Steps";
+echo $eighthDestination
+$ninthDestination = "Pantheon";
+echo $ninthDestination;
+$tenthDestination = "Catacombs";
+echo $tenthDestination;
+?>
